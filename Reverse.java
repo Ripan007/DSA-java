570f6782b8be2c80ce08d086b06132bdb81a7e2d
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2023-02-21 09:58:48 +0530

    add

--- Reverse.java
+++ Reverse.java
@@ -1,6 +1,31 @@
+import java.util.Arrays;
+
 public class Reverse {
   public static void main(String[] args) {
-    System.out.println("welcome");
+    // System.out.println("welcome");
+    int[] num = { 10, 20, 30, 40 };
+    reverse(num);
+    System.out.println(Arrays.toString(num));
+
+  }
+
+  static void reverse(int[] num) {
+    int start = 0;
+    int end = num.length - 1;
+
+    while (start < end) {
+      swap(num, start, end);
+      start++;
+      end--;
+    }
+
+  }
+
+  static void swap(int[] num, int index, int index1) {
+    int temp = num[index];
+    num[index] = num[index1];
+    num[index1] = temp;
+
   }
 
 }
--- maxValue.java
+++ maxValue.java
@@ -1,5 +1,21 @@
+// import java.util.Arrays;
+
 public class maxValue {
   public static void main(String[] args) {
-    System.out.println("welcome to maxValue");
+    // System.out.println("welcome to maxValue");
+    int num[] = { 10, 20, 30, 40, 50 };
+    // System.out.println(Arrays.toString(num));
+    System.out.println(max(num));
+  }
+
+  static int max(int[] num) {
+    int maxValue = num[0];
+    for (int i = 1; i < num.length; i++) {
+      if (num[i] > maxValue) {
+        maxValue = num[i];
+      }
+    }
+    return maxValue;
+
   }
 }
